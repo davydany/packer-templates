bf832905b8548999e0013e760d23662d9b643f74
David Daniel <davydany@gmail.com>
2019-04-12 11:44:48 -0400

    Updated MWRock's packer template to remove the clean up sxs, and fixed up the ISO check for VirtualBox Guest Additions.

--- cookbooks/packer-templates/recipes/clean_sxs.rb
+++ cookbooks/packer-templates/recipes/clean_sxs.rb
@@ -1,4 +1,4 @@
-batch 'clean SxS' do
-  code 'Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase'
-  ignore_failure true
-end
+# batch 'clean SxS' do
+#   code 'Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase'
+#   ignore_failure true
+# end
